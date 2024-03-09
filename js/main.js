6b30a198bbaf1e0ace9c4a3ddb49339f6f9c53ee
Andika maulana saputra <66026993+andikasptraa@users.noreply.github.com>
2024-03-09 17:25:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+const txtElement = ['Saputra'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efekngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 300);
+
+})();
+
+
 // Toggle & Responsive Navigation
 const navSlide = () => {
   const burger = document.querySelector(".burger");
